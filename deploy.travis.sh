618dab6e57eda3d26a8fa0a173124623dc1c888a
Charles Olivier Savignac <sircharlo@gmail.com>
2020-05-18 23:48:55 -0500

    For now, auto-build will be Mac and Linux only

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -13,8 +13,8 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
         -v ${PWD}:/project \
         -v ~/.cache/electron:/root/.cache/electron \
         -v ~/.cache/electron-builder:/root/.cache/electron-builder \
-        electronuserland/builder:wine \
-        /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux --win"
+        electronuserland/builder \
+        /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux"
     else
       yarn release
     fi
