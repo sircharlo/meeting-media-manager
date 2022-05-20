066df210663f3d13fd319b3c0853eb62f86fde19
Manoah <149895ja@gmail.com>
2022-05-20 16:54:37 +0200

    refactor: move code to seperate file

---
+++ helpers/converters.js
@@ -0,0 +1,118 @@
+// External packages
+const dayjs = require("dayjs")
+const path = require("upath")
+const fs = require("fs-extra")
+
+async function mp4Convert(perf, updateStatus, updateTile, glob, progressSet, createVideoSync, totals) {
+  perf("mp4Convert", "start");
+  updateStatus("file-video");
+  updateTile("mp4Convert", "warning");
+  var filesToProcess = glob.sync(path.join(paths.media, "*"), {
+    onlyDirectories: true
+  }).map(folderPath => path.basename(folderPath)).filter(folder => dayjs(folder, prefs.outputFolderDateFormat).isValid() && dayjs(folder, prefs.outputFolderDateFormat).isBetween(baseDate, baseDate.clone().add(6, "days"), null, "[]") && now.isSameOrBefore(dayjs(folder, prefs.outputFolderDateFormat))).map(folder => glob.sync(path.join(paths.media, folder, "*"), {
+    ignore: ["!**/(*.mp4|*.xspf)"]
+  })).flat();
+  totals.mp4Convert = {
+    total: filesToProcess.length,
+    current: 1
+  };
+  progressSet(totals.mp4Convert.current, totals.mp4Convert.total, "mp4Convert");
+  for (var mediaFile of filesToProcess) {
+    await createVideoSync(mediaFile);
+    totals.mp4Convert.current++;
+    progressSet(totals.mp4Convert.current, totals.mp4Convert.total, "mp4Convert");
+  }
+  updateTile("mp4Convert", "success");
+  perf("mp4Convert", "stop");
+}
+
+function convertPdf(mediaFile, rm, notifyUser) {
+  return new Promise((resolve)=>{
+    var pdfjsLib = require("pdfjs-dist/build/pdf.js");
+    pdfjsLib.GlobalWorkerOptions.workerSrc = require("pdfjs-dist/build/pdf.worker.entry.js");
+    pdfjsLib.getDocument({
+      url: mediaFile,
+      verbosity: 0
+    }).promise.then(async function(pdf) {
+      for (var pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+        await convertPdfPage(mediaFile, pdf, pageNum, notifyUser);
+      }
+      await rm(mediaFile);
+    }).catch((err) => {
+      notifyUser("warn", "warnPdfConversionFailure", path.basename(mediaFile), true, err);
+    }).then(() => {
+      resolve();
+    });
+  });
+}
+function convertPdfPage(mediaFile, pdf, pageNum, notifyUser) {
+  return new Promise((resolve)=>{
+    pdf.getPage(pageNum).then(function(page) {
+      $("body").append("<div id='pdf' style='display: none;'>");
+      $("div#pdf").append("<canvas id='pdfCanvas'></canvas>");
+      let scale = fullHd[1] / page.getViewport({scale: 1}).height * 2;
+      var canvas = $("#pdfCanvas")[0];
+      let ctx = canvas.getContext("2d");
+      ctx.imageSmoothingEnabled = false;
+      canvas.height = fullHd[1] * 2;
+      canvas.width = page.getViewport({scale: scale}).width;
+      page.render({
+        canvasContext: ctx,
+        viewport: page.getViewport({scale: scale})
+      }).promise.then(function() {
+        fs.writeFileSync(path.join(path.dirname(mediaFile), path.basename(mediaFile, path.extname(mediaFile)) + "-" + String(pageNum).padStart(2, "0") + ".png"), Buffer.from(canvas.toDataURL().replace(/^data:image\/\w+;base64,/, ""), "base64"));
+        $("div#pdf").remove();
+      }).catch((err) => {
+        notifyUser("warn", "warnPdfConversionFailure", path.basename(mediaFile), true, err);
+      }).finally(() => {
+        resolve();
+      });
+    });
+  });
+}
+function convertSvg(mediaFile, rm, notifyUser) {
+  return new Promise((resolve)=>{
+    $("body").append("<div id='svg'>");
+    $("div#svg").append("<img id='svgImg'>").append("<canvas id='svgCanvas'></canvas>");
+    $("img#svgImg").on("load", function() {
+      let canvas = $("#svgCanvas")[0],
+        image = $("img#svgImg")[0];
+      image.height = fullHd[1] * 2;
+      canvas.height = image.height;
+      canvas.width  = image.width;
+      let canvasContext = canvas.getContext("2d");
+      canvasContext.fillStyle = "white";
+      canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+      canvasContext.imageSmoothingEnabled = true;
+      canvasContext.imageSmoothingQuality = "high";
+      canvasContext.drawImage(image, 0, 0);
+      fs.writeFileSync(path.join(path.dirname(mediaFile), path.basename(mediaFile, path.extname(mediaFile)) + ".png"), Buffer.from(canvas.toDataURL().replace(/^data:image\/\w+;base64,/, ""), "base64"));
+      rm(mediaFile);
+      $("div#svg").remove();
+      return resolve();
+    });
+    $("img#svgImg").on("error", function() {
+      notifyUser("warn", "warnSvgConversionFailure", path.basename(mediaFile), true);
+      return resolve();
+    });
+    $("img#svgImg").prop("src", escape(mediaFile));
+  });
+}
+
+async function convertUnusableFiles(glob, rm, notifyUser) {
+  for (let pdfFile of glob.sync(path.join(paths.media, "**", "*pdf"), {
+    ignore: [path.join(paths.media, "Recurring")]
+  })) {
+    await convertPdf(pdfFile, rm, notifyUser);
+  }
+  for (let svgFile of glob.sync(path.join(paths.media, "**", "*svg"), {
+    ignore: [path.join(paths.media, "Recurring")]
+  })) {
+    await convertSvg(svgFile, rm, notifyUser);
+  }
+}
+
+module.exports = {
+  mp4Convert,
+  convertUnusableFiles,
+}
---
+++ helpers/obs.js
@@ -0,0 +1,171 @@
+// Internal packages
+const { log, notifyUser } = require('./log')
+
+// External packages
+const remote = require("@electron/remote")
+const OBSWebSocket = require("obs-websocket-js")
+
+// Variables
+let obs = {}
+let dynamicShortcuts = {}
+
+function getObs() {
+  return obs
+}
+
+function setObs(o) {
+  obs = o
+}
+
+async function obsConnect(prefs) {
+  try {
+    if (!prefs.enableObs && obs._connected) {
+      await obs.disconnect();
+      log.info("OBS disconnected.");
+      obs = {};
+    } else if (!obs._connected && prefs.enableObs && prefs.obsPort && prefs.obsPassword) {
+      obs = new OBSWebSocket();
+      obs.on("error", err => {
+        notifyUser("error", "errorObs", null, false, err);
+      });
+      obs.on("SwitchScenes", function(newScene) {
+        try {
+          if (newScene && newScene.sceneName && newScene.sceneName !== prefs.obsMediaScene) $("#obsTempCameraScene").val(newScene.sceneName).change();
+        } catch (err) {
+          log.error(err);
+        }
+      });
+      obs.on("ConnectionOpened", () => {
+        log.info("OBS success! Connected & authenticated.");
+      });
+      await obs.connect({ address: "localhost:" + prefs.obsPort, password: prefs.obsPassword }).catch(err => {
+        notifyUser("error", "errorObs", null, false, err);
+      });
+    }
+  } catch (err) {
+    notifyUser("error", "errorObs", null, false, err);
+  }
+  $(".relatedToObsScenes").toggle(!!obs._connected);
+  $(".relatedToObsLogin input").toggleClass("is-invalid", !!prefs.enableObs && !obs._connected).toggleClass("is-valid", !!prefs.enableObs && !!obs._connected);
+  return !!obs._connected;
+}
+function shortcutSet(shortcut, destination, fn) {
+  let ret = null, alreadyExists = false;
+  try {
+    if (dynamicShortcuts[destination] && dynamicShortcuts[destination].includes(shortcut)) {
+      alreadyExists = true;
+    } else if (!dynamicShortcuts[destination] || (Array.isArray(dynamicShortcuts[destination]) && !dynamicShortcuts[destination].includes(shortcut))) ret = remote.globalShortcut.register(shortcut, fn);
+    if (ret) {
+      if (!dynamicShortcuts[destination]) dynamicShortcuts[destination] = [];
+      dynamicShortcuts[destination].push(shortcut);
+    }
+    if (!(ret || alreadyExists)) {
+      notifyUser("info", "infoShortcutSetFail", shortcut);
+    }
+  } catch (err) {
+    log.error(err);
+  }
+  return ret;
+}
+function shortcutsUnset(domain) {
+  if (domain && dynamicShortcuts[domain]) for (let i = dynamicShortcuts[domain].length - 1; i >= 0; i--) {
+    try {
+      remote.globalShortcut.unregister(dynamicShortcuts[domain][i]);
+      dynamicShortcuts[domain].splice(i, 1);
+    } catch (err) {
+      log.error(err);
+    }
+  }
+}
+async function obsGetScenes(currentOnly, validateConfig, prefs) {
+  try {
+    let connectionAttempt = await obsConnect(prefs);
+    log.debug('conn', connectionAttempt)
+    return (connectionAttempt ? await obs.send("GetSceneList").then(data => {
+      log.debug('data', data)
+      if (currentOnly) {
+        return data.currentScene;
+      } else {
+        $("#overlaySettings .obs-scenes .loaded-scene").remove();
+        data.scenes.map(scene => scene.name).forEach(scene => {
+          $("#overlaySettings .obs-scenes").append($("<option>", {
+            class: "loaded-scene",
+            text: scene,
+            value: scene
+          }));
+        });
+        for (var pref of ["obsCameraScene", "obsMediaScene"]) {
+          if ($("#" + pref + " option[value='" + prefs[pref] + "']").length == 0) {
+            prefs[pref] = null;
+            validateConfig();
+          } else {
+            $("#" + pref).val(prefs[pref]);
+          }
+        }
+        $(".modal-footer .left").append("<select class='form-select form-select-lg ms-3 obs-scenes w-auto' id='obsTempCameraScene'></select>");
+        shortcutsUnset("obsScenes");
+        let cameraScenes = [];
+        $("#obsCameraScene").children().clone().filter(function (i, el) {
+          return $(el).val() && $(el).val() !== prefs.obsMediaScene;
+        }).each((i, el) => {
+          try {
+            let sceneNum = (i < 10 ? (i + 1).toString().slice(-1) : null);
+            let shortcutSetSuccess = false;
+            if ((i + 1) < 10) {
+              shortcutSetSuccess = shortcutSet("Alt+" + sceneNum, "obsScenes", function() {
+                $("#obsTempCameraScene").val($(el).val()).change();
+              });
+            }
+            cameraScenes.push({
+              id: $(el).val(),
+              text: (shortcutSetSuccess ? "<kbd class='bg-light border border-1 border-secondary fw-bold text-dark'>Alt</kbd> <kbd class='bg-light border border-1 border-secondary fw-bold text-dark'>" + sceneNum + "</kbd> - " : "") + $(el).val(),
+              html: (shortcutSetSuccess ? "<kbd class='bg-light border border-1 border-secondary fw-bold text-dark'>Alt</kbd> <kbd class='bg-light border border-1 border-secondary fw-bold text-dark'>" + sceneNum + "</kbd> - " : "") + $(el).val(),
+              title: $(el).val(),
+            });
+          } catch (err) {
+            log.error(err);
+          }
+        });
+        $("#obsTempCameraScene").select2({
+          selectionCssClass: "obsTempCameraSceneSelect",
+          dropdownParent: $("#staticBackdrop"),
+          data: cameraScenes,
+          escapeMarkup: function(markup) {
+            return markup;
+          },
+          templateResult: function(data) {
+            return data.html;
+          },
+          templateSelection: function(data) {
+            return data.text;
+          }
+        });
+        $("#obsTempCameraScene").val(data.currentScene == prefs.obsMediaScene ? prefs.obsCameraScene : data.currentScene).change();
+        return data;
+      }
+    }).catch(err => {
+      notifyUser("error", "errorObs", null, false, err);
+    }) : false);
+  } catch (err) {
+    if (obs._connected) notifyUser("error", "errorObs", null, false, err);
+    return false;
+  }
+}
+async function obsSetScene(scene, prefs) {
+  try {
+    if (await obsConnect(prefs) && scene) obs.send("SetCurrentScene", { "scene-name": scene }).catch(err => {
+      notifyUser("error", "errorObs", null, false, err);
+    });
+  } catch (err) {
+    if (obs._connected) notifyUser("error", "errorObs", null, false, err);
+  }
+}
+
+module.exports = {
+  getObs,
+  setObs,
+  obsSetScene,
+  obsGetScenes,
+  shortcutSet,
+  shortcutsUnset,
+}
