8243432e2c69ff7c8b755ae08593b024fa1540e3
Charles Olivier Savignac <sircharlo@gmail.com>
2022-05-25 10:28:58 -0400

    Added missing AUDIO_ICON and VIDEO_ICON constants

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,7 @@
-const { PREF_FIELDS } = require("./general");
+const { AUDIO_ICON, VIDEO_ICON, PREF_FIELDS } = require("./general");
 
 const { YEARTEXT, JW_API, JWPUB_API, REPO_URL, FFMPEG_VERSION } = require("./endpoints");
 
 module.exports = {
-  YEARTEXT, JW_API, JWPUB_API, REPO_URL, FFMPEG_VERSION, PREF_FIELDS
+  YEARTEXT, JW_API, JWPUB_API, REPO_URL, FFMPEG_VERSION, AUDIO_ICON, VIDEO_ICON, PREF_FIELDS
 };
