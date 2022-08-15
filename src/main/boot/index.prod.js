ac714ad1b44a550d524a2067abb0ede794d57dbc
Manoah <149895ja@gmail.com>
2022-08-15 13:26:12 +0200

    enable eslint for main/boot

--- src/main/boot/index.prod.js
+++ src/main/boot/index.prod.js
@@ -1,17 +1,17 @@
-/* eslint-disable */
-import { app, protocol } from 'electron'
-import * as path from 'path'
 import { URL } from 'url'
+import { app, protocol } from 'electron'
+import { join, normalize } from 'upath'
 
 const PRODUCTION_APP_PROTOCOL = 'app'
-const PRODUCTION_APP_PATH = path.join(__dirname, '..', 'renderer')
-
+const PRODUCTION_APP_PATH = join(__dirname, '..', 'renderer')
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: PRODUCTION_APP_PROTOCOL, privileges: { secure: true, standard: true } }
-]);
-
+  {
+    scheme: PRODUCTION_APP_PROTOCOL,
+    privileges: { secure: true, standard: true },
+  },
+])
 
 app.once('ready', () => {
   registerProtocol(PRODUCTION_APP_PROTOCOL)
@@ -19,16 +19,12 @@ app.once('ready', () => {
 
 // Credits: https://github.com/nklayman/vue-cli-plugin-electron-builder/blob/master/lib/createProtocol.js
 function registerProtocol(scheme) {
-  protocol.registerFileProtocol(
-    scheme,
-    (request, callback) => {
-      const relativePath = path.normalize(new URL(request.url).pathname)
-      const absolutePath = path.join(PRODUCTION_APP_PATH, relativePath)
+  protocol.registerFileProtocol(scheme, (request, respond) => {
+    const relativePath = normalize(new URL(request.url).pathname)
+    const absolutePath = join(PRODUCTION_APP_PATH, relativePath)
 
-      callback({ path: absolutePath })
-
-    }
-  )
+    respond({ path: absolutePath })
+  })
 }
 
 // Require `main` process to boot app
