e170b2ec18326fa5b8aa45f90d7d4ae283b90d12
Manoah <149895ja@gmail.com>
2022-08-29 12:37:25 +0200

    fix docs

--- docs/assets/js/lang.js
+++ docs/assets/js/lang.js
@@ -1,7 +1,7 @@
 const language = document.getElementById('select-lang')
 language.onchange = (e) => {
   const lang = e.target.value
-  if (!window.location.pathname.endsWith(`/${lang}/`)) {
-    window.location.href = `${lang}/`
+  if (!window.location.pathname.endsWith(`/${lang}/${window.location.hash}`)) {
+    window.location.href = `${lang}/${window.location.hash}`
   }
 }
