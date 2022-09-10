99a63ff0086a14b95d883a12a08f302966b187b0
Manoah <149895ja@gmail.com>
2022-09-10 16:20:36 +0200

    update deps

--- pre-build.sh
+++ pre-build.sh
@@ -2,6 +2,5 @@
 # Necessary sql.js file for reading sqlight databases from JWPUB files
 cp ./node_modules/sql.js/dist/sql-wasm.wasm ./src/renderer/static
 
-# Necessary pdfjs worker files for converting PDFs to images
+# Necessary pdfjs worker file for converting PDFs to images
 cp ./node_modules/pdfjs-dist/build/pdf.worker.min.js ./src/renderer/static
-cp ./node_modules/pdfjs-dist/build/pdf.worker.js.map ./src/renderer/static
