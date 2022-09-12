9d5e713bd3984eda7472dfa8f44bac6c1927957d
Manoah <149895ja@gmail.com>
2022-09-12 16:48:26 +0200

    turn on trace on retry

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,11 @@ import { PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
   testDir: './tests',
-
   timeout: 60000,
-
+  retries: process.env.CI ? 2 : 0, // set to 2 when running on CI
+  use: {
+    trace: 'on-first-retry', // record traces on first retry of each test
+  },
   expect: {
     toMatchSnapshot: { threshold: 0.2 },
   },
