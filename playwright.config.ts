b984fbbef93ec6128422d48736ca6d99e050619b
Manoah <149895ja@gmail.com>
2022-09-12 16:52:14 +0200

    update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,7 @@ const config: PlaywrightTestConfig = {
   testDir: './tests',
   timeout: 60000,
   retries: process.env.CI ? 2 : 0, // set to 2 when running on CI
+  reporter: process.env.CI ? 'dot' : 'list',
   use: {
     trace: 'on-first-retry', // record traces on first retry of each test
   },
