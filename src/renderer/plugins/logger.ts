fd3256358accfac90a643bdd5959672476a7c313
Manoah <149895ja@gmail.com>
2022-09-13 18:45:36 +0200

    clean logger

--- src/renderer/plugins/logger.ts
+++ src/renderer/plugins/logger.ts
@@ -44,12 +44,7 @@ const plugin: Plugin = ({ $getAllPrefs, $config, $sentry, store }, inject) => {
     },
     error: function (msg: any, ...args: any[]) {
       logger('error', [msg, ...args])
-      if (
-        typeof msg !== 'string' &&
-        msg?.message !== 'not connected' &&
-        msg?.code !== 'CONNECTION_ERROR' &&
-        msg?.code !== 'NOT_CONNECTED'
-      ) {
+      if (typeof msg !== 'string') {
         $sentry.captureException(msg)
       }
     },
