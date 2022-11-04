307ab72f646ef1fa5cc81a6779b816358cf70e4b
Manoah <149895ja@gmail.com>
2022-11-04 13:07:28 +0100

    correctly save test results

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,9 @@ const config: PlaywrightTestConfig = {
   reporter: process.env.CI ? 'github' : 'list',
   maxFailures: process.env.CI ? 2 : 1,
   use: {
+    screenshot: 'only-on-failure',
     trace: 'retain-on-failure', // record traces of failed tests
+    video: 'retain-on-failure', // record traces of failed tests
   },
   expect: {
     toMatchSnapshot: { threshold: 0.2 },
