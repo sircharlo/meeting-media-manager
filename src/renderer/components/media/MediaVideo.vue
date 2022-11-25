af34106fad29857d68059bc3771a5a9ec3ea09ac
Manoah <149895ja@gmail.com>
2022-11-25 08:42:04 +0100

    fix: correctly fetch additional info for videos

--- src/renderer/components/media/MediaVideo.vue
+++ src/renderer/components/media/MediaVideo.vue
@@ -193,15 +193,18 @@ export default defineComponent({
       return pathToFileURL(this.src).href + (this.thumbnail ? '' : '#t=5')
     },
     thumbnail(): string | null {
-      let thumbnail: string | null = null
+      let thumbnail: string | null
+
       const meetingMedia = this.meetings.get(this.$route.query.date as string)
       if (!meetingMedia) return null
 
       meetingMedia.forEach((media) => {
-        if (thumbnail) return
+        if (thumbnail !== undefined) return
         const file = media.find((m) => m.safeName === basename(this.src))
-        if (file) {
-          thumbnail = file.thumbnail ?? null
+        if (file?.pub?.startsWith('sjj')) {
+          thumbnail = null
+        } else if (file) {
+          thumbnail = file.thumbnail || file.trackImage || null
         }
       })
       return thumbnail
