376e93c6410d778e91f30ec985d6969347bb2a41
Manoah <149895ja@gmail.com>
2023-02-19 12:05:05 +0100

    fix: co-week locale improvements

--- src/renderer/plugins/dayjs.ts
+++ src/renderer/plugins/dayjs.ts
@@ -5,10 +5,26 @@ const { DAYJS_LOCALES } = require('~/constants/lang') as {
   DAYJS_LOCALES: string[]
 }
 
-const plugin: Plugin = ({ $dayjs }) => {
+const originalStarts = DAYJS_LOCALES.map((l) => {
+  return { lang: l, start: 1 }
+})
+
+const plugin: Plugin = ({ $dayjs }, inject) => {
   // Always start the week on Monday
   DAYJS_LOCALES.forEach((l) => {
+    const original = originalStarts.find((o) => o.lang === l)
+    if (original) {
+      original.start = $dayjs().locale(l).localeData().firstDayOfWeek()
+    }
     $dayjs.updateLocale(l, { weekStart: 1 })
+  })
+
+  inject('getFirstDayOfWeek', (lang: string) => {
+    const original = originalStarts.find((o) => o.lang === lang)
+    if (original) {
+      return original.start
+    }
+    return 1
   })
 }
 
