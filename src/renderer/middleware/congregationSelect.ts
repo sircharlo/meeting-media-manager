5fdd89d78126b264bf7d462a33df0a0a79c73c47
Charles Olivier Savignac <sircharlo@gmail.com>
2023-03-18 20:13:50 -0400

    fix: don't redirect to login on unneeded pages

--- src/renderer/middleware/congregationSelect.ts
+++ src/renderer/middleware/congregationSelect.ts
@@ -2,7 +2,9 @@ import { Middleware } from '@nuxt/types'
 
 // Open congregation picker when no congregation selected
 const middleware: Middleware = ({ store, route, redirect }) => {
-  if (!store.getters["currentCongregation/name"].name && !route.path.includes("congregation")) {
+  const pathsToCheck = ["congregation", "media", "browser"]
+  const shouldRedirect = !store.getters["currentCongregation/name"].name && !pathsToCheck.some(path => route.path.includes(path))
+  if (shouldRedirect) {
     redirect('/congregation')
     console.log("redirected to congregation picker");
   }
