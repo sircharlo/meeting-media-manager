1373b650032c2e5f4af52bde46fd5d00123f1c84
Olivier Savignac <1275666+sircharlo@users.noreply.github.com>
2024-09-06 09:38:45 -0400

    chore: add deprecation banner

--- src/renderer/components/home/HomeHeader.vue
+++ src/renderer/components/home/HomeHeader.vue
@@ -1,5 +1,40 @@
 <template>
   <v-row class="pa-4">
+    <v-col cols="12">
+      <v-alert density="compact" color="warning">
+        <h4 class="mb-4">Deprecation Notice</h4>
+        <p>
+          Please be advised that this version of Meeting Media Manager will be
+          deprecated in <b>October 2024</b>, at which point a new and much
+          improved version will be released. <b>All your settings will be
+          automatically migrated to the new version</b> when it becomes available.
+        </p>
+        <p>
+          The upcoming release features a modern, simplified and intuitive
+          interface designed for an enhanced user experience. Care has been
+          taken to ensure that the new version truly helps audio-video operators
+          to accomplish their tasks more efficiently and without distractions.
+        </p>
+        <p>
+          Note that the following features will <b>no longer be available</b> as of the
+          new release:
+        </p>
+        <ul class="mb-4">
+          <li>VLC playlist creation</li>
+          <li>Congregation WebDAV sync</li>
+          <li>Beta Zoom integration</li>
+        </ul>
+        <p>
+          If you have any questions regarding this new release, feel free to
+          <a
+            href="https://github.com/sircharlo/meeting-media-manager/discussions/new?category=q-a"
+            target="_blank"
+            rel="noopener noreferrer"
+            >reach out</a
+          >.
+        </p>
+      </v-alert>
+    </v-col>
     <v-col cols="5" sm="4" md="3" />
     <v-col cols="2" sm="4" md="6" class="text-center">
       <font-awesome-icon
