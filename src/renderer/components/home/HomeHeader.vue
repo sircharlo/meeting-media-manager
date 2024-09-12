7bc583cbda25860444b9e49be9a847a3619b2374
Olivier Savignac <1275666+sircharlo@users.noreply.github.com>
2024-09-12 08:25:38 -0400

    fix: made deprecation banner dynamic and closable

--- src/renderer/components/home/HomeHeader.vue
+++ src/renderer/components/home/HomeHeader.vue
@@ -2,37 +2,56 @@
   <v-row class="pa-4">
     <v-col cols="12">
       <v-alert density="compact" color="warning">
-        <h4 class="mb-4">Deprecation Notice</h4>
-        <p>
-          Please be advised that this version of Meeting Media Manager will be
-          deprecated in <b>October 2024</b>, at which point a new and much
-          improved version will be released. <b>All your settings will be
-          automatically migrated to the new version</b> when it becomes available.
-        </p>
-        <p>
-          The upcoming release features a modern, simplified and intuitive
-          interface designed for an enhanced user experience. Care has been
-          taken to ensure that the new version truly helps audio-video operators
-          to accomplish their tasks more efficiently and without distractions.
-        </p>
-        <p>
-          Note that the following features will <b>no longer be available</b> as of the
-          new release:
-        </p>
-        <ul class="mb-4">
-          <li>VLC playlist creation</li>
-          <li>Congregation WebDAV sync</li>
-          <li>Beta Zoom integration</li>
-        </ul>
-        <p>
-          If you have any questions regarding this new release, feel free to
-          <a
-            href="https://github.com/sircharlo/meeting-media-manager/discussions/new?category=q-a"
-            target="_blank"
-            rel="noopener noreferrer"
-            >reach out</a
-          >.
-        </p>
+        <template v-if="deprecatedShow">
+          <h3 class="mb-4">Deprecation Notice</h3>
+          <p>
+            Please be advised that this version of Meeting Media Manager
+            {{ alreadyDeprecated ? 'was' : 'will be' }} deprecated in
+            <b>October 2024</b>, at which point a new and much improved version
+            {{ alreadyDeprecated ? 'was' : 'will be' }} released.
+            <b
+              >Your current settings will be automatically migrated to the new
+              version</b
+            >
+            when
+            {{ alreadyDeprecated ? 'you install it' : 'it becomes available' }}.
+          </p>
+          <p>
+            The {{ alreadyDeprecated ? 'updated' : 'upcoming' }} release
+            features a modern, simplified and intuitive interface designed for
+            an enhanced user experience. Care has been taken to ensure that the
+            new version truly helps audio-video operators to accomplish their
+            tasks more efficiently and without distractions.
+          </p>
+          <p>
+            Note that the following features
+            <b
+              >{{
+                alreadyDeprecated ? 'are no longer' : 'will no longer be'
+              }}
+              available</b
+            >
+            in the new release:
+          </p>
+          <ul class="mb-4">
+            <li>VLC playlist creation</li>
+            <li>Congregation WebDAV sync</li>
+            <li>Beta Zoom integration</li>
+          </ul>
+          <p>
+            If you have any questions regarding this updated release, feel free
+            to
+            <a
+              href="https://github.com/sircharlo/meeting-media-manager/discussions/new?category=q-a"
+              target="_blank"
+              rel="noopener noreferrer"
+              >reach out</a
+            >.
+          </p>
+        </template>
+        <v-btn @click="deprecatedShow = !deprecatedShow"
+          >{{ deprecatedShow ? 'Hide' : 'Show' }} deprecation notice</v-btn
+        >
       </v-alert>
     </v-col>
     <v-col cols="5" sm="4" md="3" />
@@ -75,6 +94,12 @@ export default defineComponent({
       type: String,
       required: true,
     },
+  },
+  data() {
+    return {
+      deprecatedShow: true,
+      alreadyDeprecated: new Date() >= new Date('2024-11-01'),
+    }
   },
   computed: {
     isDark(): boolean {
