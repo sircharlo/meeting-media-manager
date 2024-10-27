9d59ccf014462d82a82e5d5f219b34e64b9389f5
Manoah Tervoort <149895ja@gmail.com>
2024-10-27 10:30:00 +0100

    refactor: electron process

---
+++ src-electron/window-main.ts
@@ -0,0 +1,102 @@
+import type { ElectronIpcListenKey } from 'src/types';
+
+import pkg from 'app/package.json';
+import { app, type BrowserWindow } from 'electron';
+import { existsSync, readFileSync, writeFileSync } from 'fs-extra';
+import { join } from 'upath';
+
+import { errorCatcher } from './utils';
+import { closeAllWindows, createWindow } from './window-base';
+import { createMediaWindow } from './window-media';
+
+export let mainWindow: BrowserWindow | undefined;
+export let authorizedClose = false;
+
+export function createMainWindow() {
+  // If the window is already open, just focus it
+  if (mainWindow && !mainWindow.isDestroyed()) {
+    mainWindow.show();
+    return;
+  }
+
+  // Fix Window snapping issues in window state keeper
+  fixWindowSnapping();
+
+  // Create the browser window
+  mainWindow = createWindow('main');
+
+  mainWindow.webContents.setUserAgent(
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+  );
+
+  mainWindow.on('close', (e) => {
+    if (authorizedClose) {
+      closeAllWindows();
+    } else {
+      e.preventDefault();
+      sendToMainWindow('attemptedClose');
+    }
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = undefined;
+  });
+
+  createMediaWindow();
+}
+
+export function toggleAuthorizedClose(authorized: boolean) {
+  authorizedClose = authorized;
+}
+
+export function sendToMainWindow(
+  channel: ElectronIpcListenKey,
+  ...args: unknown[]
+) {
+  mainWindow?.webContents.send(channel, ...args);
+}
+
+export function logToMainWindow(
+  msg: string,
+  ctx: Record<string, unknown> | string = {},
+  level: 'debug' | 'error' | 'info' | 'warn' = 'info',
+) {
+  if (level === 'debug' && !process.env.DEBUGGING) return;
+  sendToMainWindow('log', { ctx, level, msg });
+}
+
+// See: https://github.com/sircharlo/mmm-refactor/discussions/2#discussioncomment-10870709
+function fixWindowSnapping() {
+  try {
+    const windowStateFilePath = join(
+      app.getPath('appData'),
+      pkg.productName,
+      'window-state.json',
+    );
+
+    if (existsSync(windowStateFilePath)) {
+      const mainWindowState = JSON.parse(
+        readFileSync(windowStateFilePath, 'utf8'),
+      );
+
+      const maximumThreshold = 30;
+      const { displayBounds, height, width, x, y } = mainWindowState;
+      const overflowX = x + width - displayBounds.width - displayBounds.x;
+      const overflowY = y + height - displayBounds.height - displayBounds.y;
+
+      if (0 < overflowX && overflowX < maximumThreshold)
+        mainWindowState.width -= overflowX;
+      if (0 < overflowY && overflowY < maximumThreshold)
+        mainWindowState.height -= overflowY;
+
+      if (x < displayBounds.x && x > displayBounds.x - maximumThreshold)
+        mainWindowState.x = displayBounds.x;
+      if (y < displayBounds.y && y > displayBounds.y - maximumThreshold)
+        mainWindowState.y = displayBounds.y;
+
+      writeFileSync(windowStateFilePath, JSON.stringify(mainWindowState));
+    }
+  } catch (error) {
+    errorCatcher(error);
+  }
+}
---
+++ src/types/electron.d.ts
@@ -0,0 +1,5 @@
+export type ElectronIpcSendKey = 'authorizedClose' | 'openExternal';
+export type ElectronIpcInvokeKey = 'getVersion';
+export type ElectronIpcListenKey = 'attemptedClose' | 'log';
+
+export type ExternalWebsite = 'docs' | 'repo';
