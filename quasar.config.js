02a4de116f11762e864d7f3b0599d0120f3bd70e
Manoah Tervoort <149895ja@gmail.com>
2024-11-04 16:01:13 +0100

    chore: disable some entitlements

--- quasar.config.js
+++ quasar.config.js
@@ -104,9 +104,9 @@ module.exports = configure(function (ctx) {
         },
         mac: {
           extendInfo: {
-            'com.apple.security.cs.allow-jit': true,
-            'com.apple.security.cs.allow-unsigned-executable-memory': true,
-            'com.apple.security.cs.disable-library-validation': true,
+            //'com.apple.security.cs.allow-jit': true,
+            //'com.apple.security.cs.allow-unsigned-executable-memory': true,
+            //'com.apple.security.cs.disable-library-validation': true,
             'com.apple.security.device.audio-input': true,
             'com.apple.security.device.camera': true,
             'com.apple.security.device.microphone': true,
@@ -115,7 +115,7 @@ module.exports = configure(function (ctx) {
             NSMicrophoneUsageDescription:
               "Microphone access is required in order to use the website mirroring feature, as screen recording is treated as camera and microphone access. Please note that your device's microphone will never be accessed or used in any way by this app.",
           },
-          hardenedRuntime: false,
+          //hardenedRuntime: false,
           icon: 'icons/icon.icns',
           minimumSystemVersion: '10.15',
           publish: ['github'],
