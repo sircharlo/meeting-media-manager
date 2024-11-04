697567b0ccf00594905d49a6fd457bda91b1762a
Manoah Tervoort <149895ja@gmail.com>
2024-11-04 16:16:46 +0100

    chore: disable present website on mac again

--- src/components/ui/NavDrawer.vue
+++ src/components/ui/NavDrawer.vue
@@ -50,6 +50,7 @@
       <q-item-section>{{ $t('titles.meetingMedia') }}</q-item-section>
     </q-item>
     <q-item
+      v-if="!$q.platform.is.mac"
       v-ripple
       :active-class="navActiveClass"
       :disable="!currentSettings || invalidSettings()"
