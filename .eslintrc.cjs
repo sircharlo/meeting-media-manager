7ff147fa8b88bac58a1f8690ad45a8994bbc2aa2
Olivier Savignac <1275666+sircharlo@users.noreply.github.com>
2024-11-19 16:21:45 -0500

    fix: disable glitchy sort-intersection-types

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -104,5 +104,8 @@ module.exports = {
 
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    // perfectionist
+    'perfectionist/sort-intersection-types': 'off',
   },
 };
