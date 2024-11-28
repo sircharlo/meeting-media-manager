d0feaa88fd858dee26a1e4eda237fff00fd1dae0
Manoah Tervoort <149895ja@gmail.com>
2024-11-28 21:19:54 +0100

    chore: add documentation

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,6 +1,15 @@
 import { errorCatcher } from 'src/helpers/error-catcher';
 import { pad } from 'src/utils/general';
 
+/**
+ * Formats time in seconds to a string in the format of 'hh:mm:ss' or 'mm:ss'
+ * @param time The time in seconds
+ * @returns The formatted time string
+ * @example
+ * formatTime(3600) // '01:00:00'
+ * formatTime(60) // '01:00'
+ * formatTime(0) // '00:00'
+ */
 export const formatTime = (time: number) => {
   try {
     if (!time) return '00:00';
