1efbbf82b0c56b24989bc3c7ada7de369265c6cf
Manoah Tervoort <149895ja@gmail.com>
2024-12-05 13:04:52 +0100

    fix: module type imports

--- src-electron/utils.ts
+++ src-electron/utils.ts
@@ -1,4 +1,4 @@
-import type { ExclusiveEventHintOrCaptureContext } from '@sentry/core/build/types/utils/prepareEvent';
+import type { ExclusiveEventHintOrCaptureContext } from 'app/node_modules/@sentry/core/build/types/utils/prepareEvent';
 
 import { captureException } from '@sentry/browser';
 import { version } from 'app/package.json';
