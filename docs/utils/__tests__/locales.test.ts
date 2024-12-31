4f9a3704999715685aecc489a5c79cc424d767b0
Manoah Tervoort <149895ja@gmail.com>
2024-12-31 21:33:55 +0100

    chore: move test files

--- docs/utils/__tests__/locales.test.ts
+++ docs/utils/__tests__/locales.test.ts
@@ -2,9 +2,9 @@ import { readdir, readFile } from 'fs-extra';
 import { resolve } from 'upath';
 import { describe, expect, it } from 'vitest';
 
-import { locales as localeOptions } from './../../src/constants/locales';
-import { camelToKebabCase } from './../../src/utils/general';
-import docsMessages from './../locales';
+import { locales as localeOptions } from '../../../src/constants/locales';
+import docsMessages from '../../locales';
+import { camelToKebabCase } from '../general';
 
 describe('Locales', () => {
   it('should be defined and equal', async () => {
@@ -13,12 +13,12 @@ describe('Locales', () => {
 
     const docsLocaleMessages = Object.keys(docsMessages).sort();
 
-    const docsLocaleFiles = (await readdir(resolve(__dirname, '../locales')))
+    const docsLocaleFiles = (await readdir(resolve(__dirname, '../../locales')))
       .filter((f) => f.endsWith('.json'))
       .map((f) => f.replace('.json', ''))
       .sort();
 
-    const docsSrcFolders = (await readdir(resolve(__dirname, '../src')))
+    const docsSrcFolders = (await readdir(resolve(__dirname, '../../src')))
       .filter((f) => f !== 'assets' && f !== 'public')
       .sort();
 
@@ -32,7 +32,7 @@ describe('Locales', () => {
       localeOptions.map(async (l) => {
         const locale = camelToKebabCase(l.value);
         const page = await readFile(
-          resolve(__dirname, `../src/${locale}/index.md`),
+          resolve(__dirname, `../../src/${locale}/index.md`),
           'utf-8',
         );
 
@@ -57,19 +57,14 @@ describe('Locales', () => {
   });
 
   it('should not have unused keys', async () => {
-    const localeMapper = await readFile(
-      resolve(__dirname, '../utils/locales.ts'),
-      'utf-8',
-    );
-
-    const config = await readFile(
-      resolve(__dirname, '../.vitepress/config.mts'),
-      'utf-8',
-    );
+    const files = await Promise.all([
+      readFile(resolve(__dirname, '../locales.ts'), 'utf-8'),
+      readFile(resolve(__dirname, '../../.vitepress/config.mts'), 'utf-8'),
+    ]);
 
     const docsKeys = Object.keys(docsMessages.en);
     docsKeys.forEach((key) => {
-      expect(localeMapper + config).toContain(key);
+      expect(files.join(' ')).toContain(key);
     });
   });
 });
--- vitest.config.mts
+++ vitest.config.mts
@@ -1,6 +1,5 @@
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
@@ -9,31 +8,14 @@ import pkg from './package.json';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    quasar({
-      sassVariables: 'src/quasar-variables.scss',
-    }),
+    vue({ template: { transformAssetUrls } }),
+    quasar({ sassVariables: 'src/quasar-variables.scss' }),
     tsconfigPaths(),
   ],
-  resolve: {
-    alias: {
-      'app/': `${resolve('.')}/`,
-      'src/': `${resolve('./src')}/`,
-      'stores/': `${resolve('./src/stores')}/`,
-    },
-  },
   test: {
-    env: {
-      repository: pkg.repository.url.replace('.git', ''),
-    },
+    env: { repository: pkg.repository.url.replace('.git', '') },
     environment: 'happy-dom',
-    include: [
-      // Matches vitest tests in any subfolder of 'src' or into 'test/vitest/__tests__'
-      // Matches all files with extension 'js', 'jsx', 'ts' and 'tsx'
-      '**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
-    ],
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     setupFiles: 'test/vitest/setup-file.ts',
   },
 });
