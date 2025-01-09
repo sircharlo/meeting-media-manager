b2d1f26235e55cffced48772e3aab72e04262836
Manoah Tervoort <46671786+mtdvlpr@users.noreply.github.com>
2025-01-09 15:33:03 +0100

    feat!: improve media window title (#3819)
    * feat!: improve media window title
    
    * style: improve code style
    
    ---------
    
    Co-authored-by: Charles Olivier Savignac <1275666+sircharlo@users.noreply.github.com>

--- src/layouts/MediaPlayerLayout.vue
+++ src/layouts/MediaPlayerLayout.vue
@@ -8,7 +8,13 @@
 </template>
 
 <script setup lang="ts">
+import { useMeta } from 'quasar';
 import { onMounted } from 'vue';
+
+useMeta({
+  title: 'Media Player',
+  titleTemplate: (title) => `${title} - M³`,
+});
 
 onMounted(() => {
   document.body.style.overflow = 'hidden';
