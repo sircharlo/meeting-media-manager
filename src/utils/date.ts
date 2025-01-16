519622dce63b07bed6337abb1a5bc0441c9a40ab
Manoah Tervoort <149895ja@gmail.com>
2025-01-16 13:42:44 +0100

    test: add date util tests

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -44,12 +44,12 @@ export const isInPast = (lookupDate: Date | string, includeToday = false) => {
 
 /**
  * Converts a friendly day to a js day.
- * @param day The friendly day to convert.
- * @returns The js day.
+ * @param day The friendly day to convert. Mon-Sun = 0-6.
+ * @returns The js day. Sun-Mon = 0-6.
  */
 export const friendlyDayToJsDay = (day?: number) => {
   try {
-    if (!day) day = -1;
+    if (day === undefined) day = -1;
     const firstDay = day === 6 ? 0 : parseInt(day.toString()) + 1;
     const correctedFirstDay = firstDay > 7 ? firstDay - 7 : firstDay;
     return correctedFirstDay;
@@ -94,7 +94,7 @@ export const datesAreSame = (date1: Date | string, date2: Date | string) => {
 /**
  * Gets a specific day from a week.
  * @param lookupDate The week to get the specific day from.
- * @param desiredWeekday The desired weekday.
+ * @param desiredWeekday The desired weekday. Mon-Sun = 0-6.
  * @returns The desired date object.
  */
 export const getSpecificWeekday = (
