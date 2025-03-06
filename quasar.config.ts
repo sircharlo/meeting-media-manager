4795c74d08af9a4e7c8cec3932ecf99e9290c666
Manoah Tervoort <149895ja@gmail.com>
2025-03-06 10:07:03 +0100

    chore: revert target bumps

--- quasar.config.ts
+++ quasar.config.ts
@@ -88,7 +88,7 @@ export default defineConfig((ctx) => {
       },
       sourcemap: true,
       // See: https://www.electronjs.org/docs/latest/tutorial/electron-timelines#timeline
-      target: { browser: ['chrome134'], node: 'node22.14.0' },
+      target: { browser: ['chrome132'], node: 'node20.18.2' },
       typescript: {
         extendTsConfig: (tsConfig) => {
           tsConfig.exclude?.push('./../docs');
