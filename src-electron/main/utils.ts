e9fc786a0e545269cb7652473b25d1bbd5153443
Manoah Tervoort <149895ja@gmail.com>
2025-05-13 11:18:09 +0200

    chore: ignore http error 429

--- src-electron/main/utils.ts
+++ src-electron/main/utils.ts
@@ -145,8 +145,11 @@ export const fetchJson = async <T>(
     if (response.ok || response.status === 304) {
       return await response.json();
     } else if (
-      ![403, 404].includes(response.status) &&
-      !(response.status === 400 && params?.get('pub')?.startsWith('S-'))
+      ![403, 404, 429, 502].includes(response.status) &&
+      !(
+        response.status === 400 &&
+        ['S', 'CO'].some((p) => params?.get('pub')?.startsWith(`${p}-`))
+      )
     ) {
       captureElectronError(new Error('Failed to fetch json!'), {
         contexts: {
