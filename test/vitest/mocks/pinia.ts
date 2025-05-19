dfac8e759120eb21c3da090e725670592ad0311f
Manoah Tervoort <149895ja@gmail.com>
2025-05-19 09:51:29 +0200

    chore: remove lodash-es

--- test/vitest/mocks/pinia.ts
+++ test/vitest/mocks/pinia.ts
@@ -1,10 +1,9 @@
 import { createTestingPinia, type TestingOptions } from '@pinia/testing';
 import { config } from '@vue/test-utils';
-import { cloneDeep } from 'lodash-es';
 import { afterAll, beforeAll, vi } from 'vitest';
 
 export function installPinia(options?: TestingOptions) {
-  const globalConfigBackup = cloneDeep(config.global);
+  const globalConfigBackup = structuredClone(config.global);
 
   beforeAll(() => {
     config.global.plugins.unshift(
