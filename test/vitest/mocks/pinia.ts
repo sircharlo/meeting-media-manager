57a7cdbaf0907b075be0d661c0450e2a19e79633
Manoah Tervoort <149895ja@gmail.com>
2025-05-19 09:58:54 +0200

    Revert "chore: remove lodash-es"
    This reverts commit dfac8e759120eb21c3da090e725670592ad0311f.

--- test/vitest/mocks/pinia.ts
+++ test/vitest/mocks/pinia.ts
@@ -1,9 +1,10 @@
 import { createTestingPinia, type TestingOptions } from '@pinia/testing';
 import { config } from '@vue/test-utils';
+import { cloneDeep } from 'lodash-es';
 import { afterAll, beforeAll, vi } from 'vitest';
 
 export function installPinia(options?: TestingOptions) {
-  const globalConfigBackup = structuredClone(config.global);
+  const globalConfigBackup = cloneDeep(config.global);
 
   beforeAll(() => {
     config.global.plugins.unshift(
