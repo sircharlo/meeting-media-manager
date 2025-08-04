613ea297b50afc1439266c21d17e80a4091798cb
Olivier Savignac <1275666+sircharlo@users.noreply.github.com>
2025-08-04 13:19:35 -0400

    feat: integrate robotjs into electronApi mock

--- test/vitest/mocks/electronApi.ts
+++ test/vitest/mocks/electronApi.ts
@@ -2,6 +2,7 @@
 
 import type { ElectronApi } from 'src/types';
 
+import robot from '@jitsi/robotjs';
 import fs, { ensureDir } from 'fs-extra';
 import { fileUrlToPath, pathToFileURL, readDirectory } from 'preload/fs';
 import upath from 'upath';
@@ -120,6 +121,7 @@ export const electronApi: ElectronApi = {
   removeListeners: function (channel) {
     throw new Error('Function not implemented.');
   },
+  robot,
   setAutoStartAtLogin: function (value) {
     throw new Error('Function not implemented.');
   },
