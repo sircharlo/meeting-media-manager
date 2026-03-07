5a8a2288236c52ef4f9405046d7326ab9fdba4e4
Olivier Savignac <1275666+sircharlo@users.noreply.github.com>
2026-03-07 07:41:02 -0500

    chore: add update-langs script to ease translation availability management

--- docs/locales/index.ts
+++ docs/locales/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable perfectionist/sort-imports */
 import {
   enabled as baseEnabled,
   type LanguageValue,
@@ -7,61 +8,104 @@ export { locales as localeOptions } from '../../src/constants/locales';
 
 export const enabled: LanguageValue[] = baseEnabled.filter((l) => l !== 'en');
 
-// ! This file should import the JSON files for all languages configured in Crowdin.
+// ! This file will be updated by the update-langs script.
 
-import am from './am.json';
-import cmnHans from './cmn-hans.json';
-import de from './de.json';
-import el from './el.json';
+// 100.0% translated as of 2026-03-07
 import en from './en.json';
-import es from './es.json';
-import et from './et.json';
+
+// 99.1% translated as of 2026-03-07
+import cmnHans from './cmn-hans.json';
+
+// 98.6% translated as of 2026-03-07
 import fi from './fi.json';
+
+// 98.6% translated as of 2026-03-07
 import fr from './fr.json';
-import gaa from './gaa.json';
-import ht from './ht.json';
-import hu from './hu.json';
-import it from './it.json';
-import ko from './ko.json';
-import mg from './mg.json';
-import nl from './nl.json';
-import pl from './pl.json';
+
+// 98.1% translated as of 2026-03-07
 import ptPt from './pt-pt.json';
-import pt from './pt.json';
-import ro from './ro.json';
-import ru from './ru.json';
-import rw from './rw.json';
-import sk from './sk.json';
+
+// 98.1% translated as of 2026-03-07
 import sl from './sl.json';
+
+// 97.8% translated as of 2026-03-07
+import hu from './hu.json';
+
+// 97.8% translated as of 2026-03-07
 import sv from './sv.json';
-import sw from './sw.json';
-import tl from './tl.json';
+
+// 97.5% translated as of 2026-03-07
+import es from './es.json';
+
+// 97.5% translated as of 2026-03-07
+import et from './et.json';
+
+// 97.3% translated as of 2026-03-07
+import de from './de.json';
+
+// 97.0% translated as of 2026-03-07
+import nl from './nl.json';
+
+// 96.2% translated as of 2026-03-07
+import ru from './ru.json';
+
+// 86.1% translated as of 2026-03-07
+import it from './it.json';
+
+// 80.1% translated as of 2026-03-07
+import ko from './ko.json';
+
+// 73.4% translated as of 2026-03-07
+import pt from './pt.json';
+
+// 56.3% translated as of 2026-03-07
 import uk from './uk.json';
 
+// 50.3% translated as of 2026-03-07
+import sw from './sw.json';
+
+// 46.8% translated as of 2026-03-07
+import tl from './tl.json';
+
+// 18.0% translated as of 2026-03-07
+// import sk from './sk.json';
+
+// 6.3% translated as of 2026-03-07
+// import rw from './rw.json';
+
+// 4.6% translated as of 2026-03-07
+// import am from './am.json';
+
+// 4.6% translated as of 2026-03-07
+// import el from './el.json';
+
+// 4.4% translated as of 2026-03-07
+// import ht from './ht.json';
+
+// 4.4% translated as of 2026-03-07
+// import mg from './mg.json';
+
+// 4.3% translated as of 2026-03-07
+// import ro from './ro.json';
+
+// 0.9% translated as of 2026-03-07
+// import pl from './pl.json';
+
 const messages: Record<LanguageValue, Partial<typeof en>> = {
-  am,
   cmnHans,
   de,
-  el,
   en,
   es,
   et,
   fi,
   fr,
-  gaa,
-  ht,
   hu,
   it,
   ko,
-  mg,
   nl,
-  pl,
   pt,
   ptPt,
-  ro,
   ru,
-  rw,
-  sk,
   sl,
   sv,
   sw,
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,56 +1,108 @@
+/* eslint-disable perfectionist/sort-imports */
 import { enabled, locales } from 'src/constants/locales';
 
 export const localeOptions = locales.filter((locale) =>
   enabled.includes(locale.value),
 );
 
-// ! This file should only import the JSON files for the enabled languages.
-// ! The import statements for the disabled languages should be commented out.
+// ! This file will be updated by the update-langs script.
 
-import cmnHans from './cmn-hans.json';
-import de from './de.json';
+// 100.0% translated as of 2026-03-07
 import en from './en.json';
-import es from './es.json';
-import et from './et.json';
+
+// 99.1% translated as of 2026-03-07
+import cmnHans from './cmn-hans.json';
+
+// 98.6% translated as of 2026-03-07
+import fi from './fi.json';
+
+// 98.6% translated as of 2026-03-07
 import fr from './fr.json';
-import hu from './hu.json';
-import it from './it.json';
-import ko from './ko.json';
-import nl from './nl.json';
-import pl from './pl.json';
+
+// 98.1% translated as of 2026-03-07
 import ptPt from './pt-pt.json';
-import pt from './pt.json';
-import ru from './ru.json';
+
+// 98.1% translated as of 2026-03-07
 import sl from './sl.json';
+
+// 97.8% translated as of 2026-03-07
+import hu from './hu.json';
+
+// 97.8% translated as of 2026-03-07
 import sv from './sv.json';
-import sw from './sw.json';
-import tl from './tl.json';
+
+// 97.5% translated as of 2026-03-07
+import es from './es.json';
+
+// 97.5% translated as of 2026-03-07
+import et from './et.json';
+
+// 97.3% translated as of 2026-03-07
+import de from './de.json';
+
+// 97.0% translated as of 2026-03-07
+import nl from './nl.json';
+
+// 96.2% translated as of 2026-03-07
+import ru from './ru.json';
+
+// 86.1% translated as of 2026-03-07
+import it from './it.json';
+
+// 80.1% translated as of 2026-03-07
+import ko from './ko.json';
+
+// 73.4% translated as of 2026-03-07
+import pt from './pt.json';
+
+// 56.3% translated as of 2026-03-07
 import uk from './uk.json';
 
+// 50.3% translated as of 2026-03-07
+import sw from './sw.json';
+
+// 46.8% translated as of 2026-03-07
+import tl from './tl.json';
+
+// 18.0% translated as of 2026-03-07
+// import sk from './sk.json';
+
+// 6.3% translated as of 2026-03-07
+// import rw from './rw.json';
+
+// 4.6% translated as of 2026-03-07
+// import am from './am.json';
+
+// 4.6% translated as of 2026-03-07
+// import el from './el.json';
+
+// 4.4% translated as of 2026-03-07
+// import ht from './ht.json';
+
+// 4.4% translated as of 2026-03-07
+// import mg from './mg.json';
+
+// 4.3% translated as of 2026-03-07
+// import ro from './ro.json';
+
+// 0.9% translated as of 2026-03-07
+// import pl from './pl.json';
+
 export default {
-  // am,
   cmnHans,
   de,
-  // el,
   en,
   es,
   et,
-  // fi,
+  fi,
   fr,
-  // gaa,
-  // ht,
   hu,
   it,
   ko,
-  // mg,
   nl,
-  pl,
   pt,
   ptPt,
-  // ro,
   ru,
-  // rw,
-  // sk,
   sl,
   sv,
   sw,
